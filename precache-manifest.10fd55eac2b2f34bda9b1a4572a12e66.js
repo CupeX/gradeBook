7d67c3b6c2b06d332655f64b86f7185e607a580d
CupeX <dejan.dj@hotmail.com>
2021-01-04 19:55:20 +0100

    Updates

---
+++ precache-manifest.10fd55eac2b2f34bda9b1a4572a12e66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341eba5d829a95cb5fcb675d2b0502a7",
+    "url": "/intro-react/index.html"
+  },
+  {
+    "revision": "282f6b0c106c6b877e64",
+    "url": "/intro-react/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "2b4b8a057706c52d6877",
+    "url": "/intro-react/static/js/2.ae8e45b4.chunk.js"
+  },
+  {
+    "revision": "282f6b0c106c6b877e64",
+    "url": "/intro-react/static/js/main.44ba9c43.chunk.js"
+  },
+  {
+    "revision": "aa8e1f74143b90d1364a",
+    "url": "/intro-react/static/js/runtime~main.37ae2610.js"
+  }
+]);
